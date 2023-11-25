f711241391794f32c9f8970fb47971086b866c5f
Azazel 00 <pfbhjdfhn@mail.ru>
2023-11-25 15:55:32 +0500

    Task4

---
+++ Tyuiu.ZairovAR.Sprint5.Task4.V30.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.ZairovAR.Sprint5.Task4.V30.Lib
+{
+    public class DataService : ISprint5Task4V30
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            double res = Math.Round((Math.Pow(Convert.ToDouble(strX), 3) - Math.Tan(Convert.ToDouble(strX))) + 2.03 * Convert.ToDouble(strX), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ZairovAR.Sprint5.Task4.V30/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ZairovAR.Sprint5.Task4.V30.Lib;
+
+namespace Tyuiu.ZairovAR.Sprint5.Task4.V30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Мальсагов У.А. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Мальсагов Умар Асланович | АСОиУб-23-2                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Дан файл, в котором есть вещественное значение. Прочитать значение из   *");
+            Console.WriteLine("* файла и подставить вместо X в формуле: y = x^3 * 1.2x + 2.              *");
+            Console.WriteLine("* Округлить до трёх знаков после запятой.                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\Users\pfbhj\source\repos\Tyuiu.ZairovAR.Sprint5\Tyuiu.ZairovAR.Sprint5.Task4.V30\bin\Debug\InPutDataFileTask4V30.txt";
+
+            Console.WriteLine(" y = (x^3 - tg(x)) + 2.03x ");
+            Console.WriteLine($" Данные находятся в файле: {path}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
