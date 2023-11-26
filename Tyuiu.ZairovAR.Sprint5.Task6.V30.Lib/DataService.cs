5cceeadd607ae7d9705f339a2f996400a60691e5
Azazel 00 <pfbhjdfhn@mail.ru>
2023-11-26 13:02:50 +0500

    Task6

---
+++ Tyuiu.ZairovAR.Sprint5.Task6.V30.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.ZairovAR.Sprint5.Task6.V30.Lib
+{
+    public class DataService : ISprint5Task6V30
+    {
+        public int LoadFromDataFile(string path)
+        {
+            string r = File.ReadAllText(path).Replace(',', ' ');
+            string[] str = r.Split(' ');
+            int res = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Length == 8)
+                    res++;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ZairovAR.Sprint5.Task6.V30/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ZairovAR.Sprint5.Task6.V30.Lib;
+namespace Tyuiu.ZairovAR.Sprint5.Task6.V30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Заиров А.Р. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка файлов                                                  *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Спирин Заиров А.Р. | АСОиУб-23-2                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:/DataSprint5/InPutDataFileTask6V9.txt в котором есть набор   *");
+            Console.WriteLine("* символьных данных. Найти количество слов длинной восемь символов        *");
+            Console.WriteLine("* в заданной строке                                                       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\Users\pfbhj\source\repos\Tyuiu.ZairovAR.Sprint5\Tyuiu.ZairovAR.Sprint5.Task6.V30\bin\Debug\InPutDataFileTask6V30.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine($"Количество слов из 8 букв: {res}");
+            Console.ReadKey();
+        }
+    }
+}
